e6ade03defc78a543882bc75979516987cac5d79
Michael Housh <mhoush@houshhomeenergy.com>
2016-03-08 11:31:40 -0500

    Added form elements and test, working on coverage.

--- bootstrap_wrapper/forms.py
+++ bootstrap_wrapper/forms.py
@@ -5,10 +5,10 @@
         Holds form elements.
 """
 from dominate.util import raw
-from dominate.tags import br
+from dominate.tags import br, p
 
 from .helpers import KDep, KClassDep, KDefault, KClassDefault
-from .elements import Tag
+from .elements import Tag, Div
 
 
 class BootstrapForm(Tag):
@@ -24,3 +24,86 @@ class BootstrapForm(Tag):
         self.krole = KDep('form', key='role')
 
         super().__init__(*items, **self.update_kwargs(kwargs))
+
+
+class QuickForm(BootstrapForm):
+    """ A helper to render a WTForms Form. """
+    
+    def __init__(self, form=None, pull_right=False, **kwargs):
+
+        super().__init__(**kwargs)
+
+        self.form = form
+        self.pull_right = pull_right
+
+        if self.form is not None:
+            try:
+                self.add(raw(form.hidden_tag()))
+            except:
+                pass
+
+            for field in self.form:
+                if field.widget.input_type is not 'hidden' and \
+                        field.widget.input_type is not 'submit':
+                    self.add(FormGroup(field=field))
+
+                elif field.widget.input_type is 'submit':
+                    button_class = 'btn btn-primary'
+                    if self.pull_right is True:
+                        button_class += ' pull-right'
+                    self.add(raw(field(class_=button_class)))
+
+
+class FormGroup(Div):
+    """ A Form Group holds a label, field, and any errors associated with a field.
+        This is primarily used with a WTForm field.
+    """
+
+    def __init__(self, *args, field=None, **kwargs):
+        self.kclass_dep = KClassDep('form-group')
+        self.kclass_default = KClassDefault()
+        if field is not None:
+            if field.errors:
+                self.kclass_default.append('has-error')
+            if field.flags.required:
+                self.kclass_default.append('required')
+
+        class_args = kwargs.pop('_class', None) or kwargs.pop('cls', None) or kwargs.pop('class_', None)
+        if class_args is not None:
+            self.kclass_default.append(class_args)
+
+        super().__init__(**self.update_kwargs(kwargs))
+
+        if field is not None:
+            self.add(
+                raw(str(field.label)),
+                br(),
+                raw(field(class_='form-control')),
+            )
+
+            if field.errors:
+                for error in field.errors:
+                    self.add(p(error, _class='help-block'))
+
+        # should check/do something with *args, but not sure what at the moment,
+        # possibly just remove the possibility to pass args in, and make this
+        # an element only responsible for WTForm field's.
+
+
+class FormField(Tag):
+    """ A basic form field. """
+    tagname = 'input'
+    is_single = True
+
+    def __init__(self, type, id, place_holder=None, **kwargs):
+
+        self.kclass_dep = KClassDep('form-control')
+        self.ktype = KDep(type, key='type')
+        self.kid = KDep(id, key='id')
+
+        self.kplace_holder = KDefault(key='placeholder')
+        if place_holder is not None:
+            self.kplace_holder.append(place_holder)
+
+        super().__init__(**self.update_kwargs(kwargs))
+                    
--- tests/forms_test.py
+++ tests/forms_test.py
@@ -1,12 +1,92 @@
 from unittest import TestCase
-
 from dominate.tags import *
+from wtforms import Form as _WForm
+from wtforms.fields import *
 
 from bootstrap_wrapper.forms import *
 
 class FormsTestCase(TestCase):
 
-    def test_bootstrap_form(self):
-        f = BootstrapForm(action='/test')
-        tv = form(role='form', action='/test', method='POST')
+    def test_form_group_tag(self): #{{{
+        class F(_WForm):
+            name = StringField('Name')
+
+        for field in F():
+            f = FormGroup(field=field)
+            tv = div(raw(str(field.label)), br(), raw(str(field(class_='form-control'))), _class='form-group')
+
         self.assertEqual(f.render(), tv.render())
+#}}}
+    def test_quick_form(self): #{{{
+        class F(_WForm):
+            name = StringField('Name')
+            email = StringField('Email')
+            submit = SubmitField()
+
+        f = QuickForm(F())
+
+        tv = form(
+                div(
+                    label('Name', _for='name'),
+                    br(),
+                    input(type='text',_class='form-control', id='name', name='name', value=''),
+                    _class='form-group',
+                ),
+                div(
+                    label('Email', _for='email'),
+                    br(),
+                    input(type='text', _class='form-control', id='email', name='email', value=''),
+                    _class='form-group'
+                ),
+                input(
+                    _class="btn btn-primary",
+                    id="submit",
+                    name="submit",
+                    type="submit",
+                    value="Submit"
+                ),
+                method='POST',
+                role='form'
+        )
+        
+        # must be rendered inline for tests because of adding field's as raw tags inside
+        # our WTForm tag doesn't add the same newlines and tabs
+        self.assertEqual(f.render(inline=True), tv.render(inline=True))
+
+        f = QuickForm(F(), pull_right=True)
+        tv = form(
+                div(
+                    label('Name', _for='name'),
+                    br(),
+                    input(type='text',_class='form-control', id='name', name='name', value=''),
+                    _class='form-group',
+                ),
+                div(
+                    label('Email', _for='email'),
+                    br(),
+                    input(type='text', _class='form-control', id='email', name='email', value=''),
+                    _class='form-group'
+                ),
+                input(
+                    _class="btn btn-primary pull-right",
+                    id="submit",
+                    name="submit",
+                    type="submit",
+                    value="Submit"
+                ),
+                method='POST',
+                role='form'
+        )
+#}}}
+    def test_form_field(self): #{{{
+        f = FormField('text', 'name', place_holder='Enter Your Name')
+        tv = input(type='text', id='name', placeholder='Enter Your Name', _class='form-control')
+        self.assertEqual(f.render(), tv.render())
+#}}}
+    def test_bootstrap_form_tag(self): #{{{
+        f = BootstrapForm()
+        tv = form(method='POST', role='form')
+        self.assertEqual(f.render(), tv.render())
+#}}}
+
+
