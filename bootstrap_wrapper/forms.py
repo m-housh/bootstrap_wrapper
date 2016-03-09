ca99cacb94fa86d403a3f8aaf23d69a70a398f67
Michael Housh <mhoush@houshhomeenergy.com>
2016-03-09 14:26:13 -0500

    Updated form-row to add offset

--- bootstrap_wrapper/forms.py
+++ bootstrap_wrapper/forms.py
@@ -4,11 +4,13 @@
 
         Holds form elements.
 """
+import sys
 from dominate.util import raw
 from dominate.tags import br, p, div
-from wtforms.fields import SubmitField
+from functools import reduce
+from wtforms.fields import SubmitField, SelectMultipleField
 
-from .helpers import KDep, KClassDep, KDefault, KClassDefault
+from .helpers import KDep, KClassDep, KDefault, KClassDefault, KStyle
 from .elements import Tag, Div
 
 
@@ -57,15 +59,21 @@ class QuickForm(BootstrapForm):
 class FormRow(Div):
     """ Allows the creation of row with form fields inline with labels above them. """
 
-    def __init__(self, *fields, col_args=None, **kwargs):
+    def __init__(self, *fields, col_args=None, offset=None, **kwargs):
         self.kclass_dep = KClassDep('row')
+        self.kstyle = KStyle('padding-bottom: 10px;', 'padding-top:20px;')
         super().__init__(**self.update_kwargs(kwargs))
 
         def _col(n):
             return 'col-sm-{}'.format(n)
-        
+
+        if offset is not None:
+            self.add(div(_class=_col(offset)))
+
+       
         count = 0
-        while count < len(fields):
+        while count < len(fields):  
+            # _kwargs get passed to the form group or div if it's a submit field
             _kwargs = {}
             if count < len(col_args):
                 _kwargs['_class'] = _col(col_args[count])
@@ -73,11 +81,13 @@ class FormRow(Div):
             _kwargs['style'] = 'float:left;'
 
             field = fields[count]
+
             if isinstance(field, SubmitField):
                 _kwargs['style'] += 'padding-top:25px;'
                 self.add(div(raw(field(class_='btn btn-primary')), **_kwargs))
             else:
                 self.add(FormGroup(field=field, **_kwargs))
+
             count += 1
 
                 
