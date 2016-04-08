bcebdbdc204e25d4c88b23e8420b55b5f5c2d805
Michael Housh <mhoush@houshhomeenergy.com>
2016-04-08 12:56:04 -0400

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+        name='Bootstrap Wrapper',
+        version='0.1.0',
+        description='Dominate Bootstrap Wrapper',
+        author='Michael Housh',
+        author_email='mhoush@houshhomeenergy.com',
+        url='https://github.com/m-housh/bootstrap_wrapper.git',
+        packages=['bootstrap_wrapper'],
+        requires=['dominate'],
+    )
+
