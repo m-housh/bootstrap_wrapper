6e32f6d4417027a7a49dfe021105939cf9c3a41b
Michael Housh <mhoush@houshhomeenergy.com>
2016-04-16 14:17:03 -0400

    Updated import statements to include the A element

--- bootstrap_wrapper/__init__.py
+++ bootstrap_wrapper/__init__.py
@@ -1,7 +1,7 @@
 from .document import Document, BootstrapDocument
 from .elements import ElementMeta, Element, Tag, Div, Ul, \
     Glyphicon, DropdownButton, Dropdown, TableHeader, TableRow, TableBody, \
-    Table, ResponsiveTable, Button, Small
+    Table, ResponsiveTable, Button, Small, A
 from .navigation import Tabbar, Tab, TabbarTab, TabContent, TabPane, TabbarUl, \
     NavbarBrand, NavbarItems, NavbarDropdown, Navbar 
 from .forms import BootstrapForm, QuickForm, FormRow, FormGroup, FormField
@@ -12,4 +12,4 @@ __all__ = ['Document', 'BootstrapDocument', 'ElementMeta', 'Element', 'Tag', 'Di
         'Table', 'ResponsiveTable', 'Button', 'Small', 'Tabbar', 'Tab', 'TabbarTab', \
         'TabContent', 'TabPane', 'TabbarUl', 'NavbarBrand', 'NavbarItems', \
         'NavbarDropdown', 'Navbar', 'BootstrapForm', 'QuickForm', 'FormRow', 'FormGroup', \
-        'FormField']
+        'FormField', 'A']
