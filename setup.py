fc6e8dab26be92bd34732efece102bfb70d00302
Michael Housh <mhoush@houshhomeenergy.com>
2016-05-03 21:53:49 -0400

    Moved readme.md to readme.rst

--- setup.py
+++ setup.py
@@ -1,9 +1,14 @@
-from distutils.core import setup
+from setuptools import setup
+
+__version__ = '0.1.0'
 
 setup(
-        name='Bootstrap Wrapper',
+        name='BootstrapWrapper',
         version='0.1.0',
+
         description='Dominate Bootstrap Wrapper',
+        long_description=open('README.md').read(),
+
         author='Michael Housh',
         author_email='mhoush@houshhomeenergy.com',
         url='https://github.com/m-housh/bootstrap_wrapper.git',
